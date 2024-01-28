ac79fd7a4c5490b057580c4fbc5b04f0509ed840
YaninaNGarofalo <yanina.natalia.garofalo@gmail.com>
2024-01-28 19:56:09 -0300

    YG - firebase integration

---
+++ firebase/firebase.config.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAolH8xkPrzpqsFD0R2jTAfiMngNNBuUHk",
+  authDomain: "nextjs-tool.firebaseapp.com",
+  projectId: "nextjs-tool",
+  storageBucket: "nextjs-tool.appspot.com",
+  messagingSenderId: "8586546563",
+  appId: "1:8586546563:web:f4cb3d6c745337c253fab6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
